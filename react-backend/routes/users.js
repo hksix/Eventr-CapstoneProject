89ed430d7779746b9f602d941451b1778b9c739d
hksix <mh.haseeb@gmail.com>
2017-09-26 12:05:19 -0400

    Adds nodemon

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
   res.json([{
   	id: 1,
-  	username: "Test"
+  	username: "YAYYY"
   }, {
   	id: 2,
   	username: "User"
