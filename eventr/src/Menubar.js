b53328db99861bbc61bb35af24508ef653bc6f77
Stephanie Asmar <sasmar8@gmail.com>
2017-09-27 10:16:20 -0400

    Fixes minor syntax error

--- eventr/src/Menubar.js
+++ eventr/src/Menubar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-import { CSSTransitionGroup } from 'react-transition-group'
+import { CSSTransitionGroup } from 'react-transition-group';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './MyAwesomeReactComponent';
