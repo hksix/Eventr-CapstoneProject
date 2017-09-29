5383c83be2b75cd46ef257b7fbbf4f5269f75eb8
Stephanie Asmar <sasmar8@gmail.com>
2017-09-29 11:05:48 -0400

    steph's item registry commits

--- eventr/src/App.js
+++ eventr/src/App.js
@@ -5,6 +5,7 @@ import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import ItemRegistry from './itemRegistry';
 
 
 import Thing from './Menubar.js'
@@ -21,6 +22,13 @@ class App extends Component {
     return (
       <div className="App">
         <Thing />
+        <div>
+          <form className="App" onSubmit={this.onSubmit}>
+            <input value={this.state.term} onChange={this.onChange} />
+            <button>Submit</button>
+          </form>
+          <ItemRegistry items={this.state.items} />
+        </div>
        <h1>Users</h1>
        {this.state.users.map(user=>
        <div key={user.id}>{user.username}{user.id}</div>)}
