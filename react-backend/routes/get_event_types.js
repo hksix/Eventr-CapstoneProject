d8572bf9abf3578a16777e459f892c20a85141d3
jenlij <jljohns1216@gmail.com>
2017-09-30 23:19:04 -0400

    trying to connect to DB

--- react-backend/routes/get_event_types.js
+++ react-backend/routes/get_event_types.js
@@ -4,21 +4,24 @@ const db = require('../db');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HELLO' });
-});
-
-
 // router.get('/', function(req, res, next) {
-//     db.query(`
-//       select * from users;
-//     `)
-//       .then((result)=>{
-//         res.render('index',{
-//           users: result
-//         });
-//       });
+//   res.render('event_types', { title: 'HELLO' });
 // });
+
+
+router.get('/', function(req, res, next) {
+    db.query(`
+      select * from event_categories;
+    `)
+      .then((result)=>{
+        res.render('event_types',{
+          users: result
+        })
+        .catch((error) => {
+
+          });
+      });
+});
 
 
 module.exports = router;
