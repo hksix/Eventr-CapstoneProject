8604e2dc910aba5f198c89aab451b48c81a8a707
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2017-10-01 22:58:30 +0100

    Update get_tables_data.js

--- react-backend/routes/get_tables_data.js
+++ react-backend/routes/get_tables_data.js
@@ -3,10 +3,9 @@ var router = express.Router();
 const db = require('../db');
 
 router.get('/:table/:order', function(req, res, next) {
-    db.any('select * from ${table:name} ORDER BY ${order:raw};', req.params)
-      .then((results)=>{
+    db.any('select * from ${table:name} ORDER BY ${order:raw}', req.params)
+      .then(results => {
         res.json(results)
-
       });
 });
 
