eb971901a0e9a0c758d97f07cd4b3e903d31d951
Katie <katherinelane11@gmail.com>
2017-10-04 15:06:19 -0400

    adds dummy data to events and event categories

--- restify-api/seeders/20171004181153-events.js
+++ restify-api/seeders/20171004181153-events.js
@@ -2,16 +2,52 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Events', [{
+      event_id: 1,
+      host_id: '',
+      name: 'Birthday Party',
+      description: 'Come celebrate Stephanie\'s birthday!',
+      date: 2017-10-25,
+      location: '999 Belle Isle Cir NE Atlanta GA 30329',
+      category_id: '',
+    },
+    {
+      event_id: 2,
+      host_id: '',
+      name: 'Family Celebration',
+      description: 'Come celebrate Grandma\'s 90th birthday!',
+      date: 2017-11-20,
+      location: '2222 Belle Isle Cir NE Atlanta GA 30329',
+      category_id: '',
+    },
+    {
+      event_id: 3,
+      host_id: '',
+      name: 'Dance Party',
+      description: 'Let\'s dance!',
+      date: 2017-12-05,
+      location: '444 Belle Isle Cir NE Atlanta GA 30329',
+      category_id: '',
+    },
+    {
+      event_id: 4,
+      host_id: '',
+      name: 'Beach Weekend',
+      description: 'Renting a beachoutse for the weekend and excited to see everyone',
+      date: 2018-01-25,
+      location: '404 East Bay St Savannah GA 31401',
+      category_id: '',
+    },
+    {
+      event_id: 5,
+      host_id: '',
+      name: 'Family Reunion',
+      description: 'Celebrating our family',
+      date: 2017-10-25,
+      location: '1212 Belle Isle Cir NE Atlanta GA 30329',
+      category_id: '',
+    }
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
