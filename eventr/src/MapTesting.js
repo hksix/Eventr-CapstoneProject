0f888e84e049ef5a1ccd0f866010c39af187cdfe
Katie <katherinelane11@gmail.com>
2017-10-04 11:56:45 -0400

    adding listeners to map

--- eventr/src/MapTesting.js
+++ eventr/src/MapTesting.js
@@ -99,20 +99,86 @@ export default class Map extends Component {
 
   initMap() {
     const googleInterval = setInterval(function(){
-      console.log("workssssss")
-      console.log(`der googles is`);
       console.log(window.google);
       
       if (window.google && window.google.maps){
         const GMap = window.google.maps.Map;
-        console.log(GMap);
-
-        console.log("inside stffssese?")
+        var markers = [];
         try {
+          // had to use document.getElementById because the asynconous of google maps and refs were not allowing the map to render to the page 
           var map = new GMap(document.getElementById('map'), {
             center: {lat: -33.8688, lng: 151.2195},
             zoom: 13,
           });
+          var input = document.getElementById('input')
+          var searchBox = new window.google.maps.places.SearchBox(input);
+          map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(input);
+          map.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.getBounds());
+          });
+
+          searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+        
+            if (places.length == 0) {
+              return;
+            }
+            // Clear out the old markers.
+            markers.forEach(function(marker) {
+              marker.setMap(null);
+            });
+            
+            markers = [];
+        
+            
+            // For each place, get the icon, name and location.
+            var bounds = new window.google.maps.LatLngBounds();
+            var infos = [];
+            places.forEach(function(place) {
+              if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+              }
+              var icon = {
+                url: place.icon,
+                size: new window.google.maps.Size(71, 71),
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(17, 34),
+                scaledSize: new window.google.maps.Size(25, 25)
+              };
+        
+              var marker = new window.google.maps.Marker({
+                map: map,
+                icon: icon,
+                name: place.name,
+                position: place.geometry.location,
+                address: place.formatted_address,
+              });
+              console.log(place)
+              var infowindow = new window.google.maps.InfoWindow();
+              window.google.maps.event.addListener(marker, 'click', (function(marker, infowindow) {
+                return function() {
+                  if (infos.length > 0) {
+                    infos[0].set("marker", null);
+                    infos[0].close();
+                    infos.length = 0;
+                  }
+                  infowindow.setContent('<div><strong>' + marker.name + '</strong><br>' + marker.address + '</div>');
+                  infowindow.open(map, marker);
+                  infos[0] = infowindow;
+                };
+              })(marker, infowindow));
+        
+              if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+              } else {
+                bounds.extend(place.geometry.location);
+              }
+            });
+            map.fitBounds(bounds);
+          });
+
           console.log("hi!")
           // console.log(window.map)  
           clearInterval(googleInterval)
@@ -122,6 +188,7 @@ export default class Map extends Component {
       }
     }, 1000)
   }
+
     
 
   
@@ -141,6 +208,7 @@ export default class Map extends Component {
          }}
          >Map should be here
         </div>
+        <input id="input"></input>
       </div>
     )
   }
