db1f96ee1e4cc509ff266a319c44163c86b70e17
Katie <katherinelane11@gmail.com>
2017-10-04 15:25:26 -0400

    data for event inventory

--- restify-api/seeders/20171004181153-events.js
+++ restify-api/seeders/20171004181153-events.js
@@ -9,7 +9,7 @@ module.exports = {
       description: 'Come celebrate Stephanie\'s birthday!',
       date: 2017-10-25,
       location: '999 Belle Isle Cir NE Atlanta GA 30329',
-      category_id: '',
+      category_id: 1,
     },
     {
       event_id: 2,
@@ -18,7 +18,7 @@ module.exports = {
       description: 'Come celebrate Grandma\'s 90th birthday!',
       date: 2017-11-20,
       location: '2222 Belle Isle Cir NE Atlanta GA 30329',
-      category_id: '',
+      category_id: 4,
     },
     {
       event_id: 3,
@@ -27,7 +27,7 @@ module.exports = {
       description: 'Let\'s dance!',
       date: 2017-12-05,
       location: '444 Belle Isle Cir NE Atlanta GA 30329',
-      category_id: '',
+      category_id: 3,
     },
     {
       event_id: 4,
@@ -36,7 +36,7 @@ module.exports = {
       description: 'Renting a beachoutse for the weekend and excited to see everyone',
       date: 2018-01-25,
       location: '404 East Bay St Savannah GA 31401',
-      category_id: '',
+      category_id: 1,
     },
     {
       event_id: 5,
@@ -45,7 +45,7 @@ module.exports = {
       description: 'Celebrating our family',
       date: 2017-10-25,
       location: '1212 Belle Isle Cir NE Atlanta GA 30329',
-      category_id: '',
+      category_id: 4,
     }
     ])
   },
