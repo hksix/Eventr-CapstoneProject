fb43b252d9468dbfba7860e39a3e65899447f6c8
Katie <katherinelane11@gmail.com>
2017-10-13 23:19:29 -0400

    adds description to selected default items

---
+++ eventr/src/EventTypeDefaultItems.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+
+import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';       
+import Checkbox from 'material-ui/Checkbox';     
+
+
+
+const styles = {
+  checkbox: {
+    marginBottom: 16,
+    width: 150,
+    // paddingRight: 20,
+    // borderColor: '#000',
+    // borderWidth: 2,
+    // borderStyle: 'solid'
+  },
+  checkboxContainer: {
+    maxHeight: 400,
+    overflow: 'scroll',
+    // borderColor: '#FF5733',
+    // borderWidth: 2,
+    // borderStyle: 'solid'
+  },
+  textField: {
+    width: 250,
+    // borderColor: '#808080',
+    // borderWidth: 2,
+    // borderStyle: 'solid'
+  }
+};
+
+
+export default class EventTypeDefaultItems extends Component {
+  constructor(props){
+      super(props);
+      
+  this.state = {
+    defaultItems: [],
+    };
+  }
+  componentWillMount = () => {
+    this.selectedCheckboxes = new Set();
+  }
+  
+  // used this function over material ui updateCheck because on click was causing all items to be checked
+  //http://react.tips/checkboxes-in-react/ 
+  toggleCheckbox = (label) => {
+    // checks box
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label);
+      console.log(label)
+    } else {
+      //unchecks box
+      this.selectedCheckboxes.add(label);
+      console.log(label)
+      }
+  }
+
+  
+  render() {
+    return (
+      <div style={styles.checkboxContainer}>  
+
+        {/* maps through database of items attached to types of events to then populate certain items associated with event */}
+        {this.props.defaultItems.map((item, indx) => (
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
+          <Checkbox 
+            label={item} 
+            key={item} 
+            onCheck={this.toggleCheckbox}
+            style={styles.checkbox}/>
+            <TextField
+              floatingLabelText="Description"
+              style={styles.textField}
+              onChange={this._handleChangeName}
+            >
+            </TextField>
+            <Divider />
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+          
+}
--- eventr/src/SetEvent.js
+++ eventr/src/SetEvent.js
@@ -6,7 +6,7 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 
-import ItemList from './ListOfItems.js'
+import ItemList from './ListOfItems.js';
 // import SubmitSnackBar from './Submit.js'
 import Card from 'material-ui/Card';
 import { ROOT_URL } from './App.js';
@@ -16,10 +16,11 @@ import axios from 'axios';
 // import RaisedButton from 'material-ui/RaisedButton';
 
 
-import {DateSelector, TimeSelector, LocationSelector} from './setevent/WhenWhere.js'
-import {NameSelector, DescriptionSelector} from './setevent/NameDesc.js'
-import PartyTypeTable  from './setevent/EventType.js'
-import { SelectFriends} from './setevent/SelectFriends.js'
+import {DateSelector, TimeSelector, LocationSelector} from './setevent/WhenWhere.js';
+import {NameSelector, DescriptionSelector} from './setevent/NameDesc.js';
+import PartyTypeTable  from './setevent/EventType.js';
+import { SelectFriends} from './setevent/SelectFriends.js';
+import EventTypeDefaultItems from './EventTypeDefaultItems';
 // google maps drawer for example
 // request invite
 // public event toggle - radius from current location  but doesnt show actual location // trending parties in area 
@@ -156,7 +157,7 @@ export class SetEvent extends Component {
       eventID:'',
       eventPeopleCount:'',
       eventPeopleNames:'',
-      eventItems:'',
+      eventItems:[],
     };
   }
 
@@ -224,12 +225,13 @@ export class SetEvent extends Component {
             changeHandler={this._handleInviteChange}/>
             
         </div>
-
+          
         <div style={styles.slide}>
             <PartyTypeTable type={this.state.eventType}
-            changeHandler={this._handleTypeChange}/>
+            changeHandler={this._handleTypeChange} autoWidth={false}/>
+            <EventTypeDefaultItems defaultItems={this.state.eventItems}/>
             <h2 style={styles.headline}>Items page</h2>
-            <ItemList defaultItems={this.state.eventItems}/> 
+            <ItemList /> 
         </div>
 
         <div style={styles.slide}>
