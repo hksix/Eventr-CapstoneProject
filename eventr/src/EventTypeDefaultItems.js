ebcf576496a478ce3f334366fe874b60ccb79127
Katie <katherinelane11@gmail.com>
2017-10-14 16:50:31 -0400

    fixes bug of toggle checkbox with value and event

--- eventr/src/EventTypeDefaultItems.js
+++ eventr/src/EventTypeDefaultItems.js
@@ -30,6 +30,7 @@ const styles = {
   }
 };
 
+const itemsForEvent = []
 
 export default class EventTypeDefaultItems extends Component {
   constructor(props){
@@ -45,6 +46,7 @@ export default class EventTypeDefaultItems extends Component {
   }
   
   render() {
+    
     return (
       <div style={styles.checkboxContainer}>  
 
@@ -52,9 +54,11 @@ export default class EventTypeDefaultItems extends Component {
         {this.props.defaultItems.map((item, indx) => (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
           <Checkbox 
-            label={item} 
+            label={item}
+            value={item} 
             key={item.index} 
             onCheck={this._toggleCheckbox}
+              
             style={styles.checkbox}/>
             <TextField
               floatingLabelText="Description"
@@ -67,18 +71,33 @@ export default class EventTypeDefaultItems extends Component {
       </div>
     )
   }
-
-    // used this function over material ui updateCheck because on click was causing all items to be checked
+  
+  // used this function over material ui updateCheck because on click was causing all items to be checked
   //http://react.tips/checkboxes-in-react/ 
-  _toggleCheckbox = (label) => {
+  // ************ DO NOT CHANGE ORDER OF ARGUMENTS BEING PASSED INTO FUNCTION ************
+  // ************ LABEL is NOT the same lable that is given to the checkbox. Label for this function is the event of the checkbox being handled
+  // ************ DO NOT CHANGE ORDER OF IF STATEMENTS
+  // had to set up two different if statements before taking care of label for checkboxes because label interfears with event and value
+  // value is a boolian when used in correct order
+  // event.target.value is value (item name) of the checkbox
+  _toggleCheckbox = (event, value, label) => {
+
+    if (value === true){
+      itemsForEvent.push(event.target.value)
+      console.log(itemsForEvent)
+    } if (value === false){
+      // splice is not the correct function to use, but will change later (katie 10/14)
+      itemsForEvent.splice(event.target.value)
+      console.log(itemsForEvent)
+    }
+
+    // ********** Handles checkbox function. Do not mess with this. Label interfears with many other aspects of the checkbox
     // checks box
     if (this.selectedCheckboxes.has(label)) {
       this.selectedCheckboxes.delete(label);
-      console.log(label)
     } else {
       //unchecks box
       this.selectedCheckboxes.add(label);
-      console.log(label)
       }
   }
 
