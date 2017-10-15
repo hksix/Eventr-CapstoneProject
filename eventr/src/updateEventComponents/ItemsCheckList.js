204b0b2db19a83172fb42568bf3d5f25e268dd8e
Katie <katherinelane11@gmail.com>
2017-10-15 16:38:30 -0400

    adjusting items to be rendered with props being passed in

--- eventr/src/updateEventComponents/ItemsCheckList.js
+++ eventr/src/updateEventComponents/ItemsCheckList.js
@@ -2,112 +2,134 @@ import React, {Component} from 'react'
 
 import _ from 'lodash'
 
-class NewTodo extends Component {
-    create(event) {
+class NewItem extends Component {
+    createNewItemForEvent = (event) => {
         event.preventDefault();
-        let text = this.refs.newTodoText.value;
+        let text = this.refs.NewItemText.value;
+        let description = this.refs.NewItemDescription.value;
+        let quantity = this.refs.NewItemQuantity.value;
         if (text) {
-            this.props.createTask(text);
-            this.refs.newTodoText.value = '';
+            this.props.createItem(text, quantity, description);
+            this.refs.NewItemText.value = '';
+            this.refs.NewItemDescription.value = '';
+            this.refs.NewItemQuantity.value = '';
         }
     }
-    
+    // ability to add more items to event from welcome page
     render() {
         return (
-            <form onSubmit={this.create.bind(this)}>
-                <label>Add more</label> <input type="text" ref="newTodoText"/> <button type="submit">+</button>
+            <form onSubmit={this.createNewItemForEvent}>
+                <label>Add more</label> 
+                <input type="text" ref="NewItemText" placeholder="Item"/> 
+                <input type="text" ref="NewItemDescription" placeholder="Description"/>
+                <input type="text" ref="NewItemQuantity" placeholder="Qty" style={{width: 30}}/>
+                <button type="submit">+</button>
             </form>
         );
     }
 }
 
-class TodoList extends Component {
-    render() {
-        let items = [];
-        let todos = _.sortBy(this.props.todos, 'done');
-        
-        for (let index in todos) {
-            items.push(
-                <Todo todo={todos[index]} key={index} toggle={this.props.toggle}/>
-            );
+// passes props to item Class to render list of items to DOM
+class ItemList extends Component {
+    componentWillReceiveProps = (nextProps) => {
+
+    }
+    itemCheckedOff = (event) => {
+        event.preventDefault();
+        this.props.toggle(this.props.item);
+    }
+    itemsToBeRendered = (items) => {
+        let itemList = []
+        console.log(items)
+        if(items.length > 0){
+            itemList = items.map((item, index) => {
+                return (
+                    <div>
+                    <div key={item.index}>
+                        {item.quantity} 
+                        {item.itemname}: 
+                        {item.description}
+                    </div> 
+                    <a href="" onClick={this.itemCheckedOff}>✓</a>
+                    <small style={{color:'green'}}>Hamza</small>
+                    </div>
+            )})
+        } else {
+            itemList.push(<li>No items added to event</li>)
         }
-        
-        return (
-            <ul style={{listStyleType: "circle"}}>
-                {items}
+        return(
+            <ul>
+                {itemList}
             </ul>
+        )
+        
+    }
+    
+    render() {
+        console.log(this.props.items)
+        return (
+            <div>
+                {this.itemsToBeRendered(this.props.items)}
+
+            </div>
         );
     }
 }
 
-class Todo extends Component {
-    done(event) {
-        event.preventDefault();
-
-        this.props.toggle(this.props.todo);
-    }
-
+class Item extends Component {
     render() {
-        let todo = this.props.todo;
-        if (todo.done) {
-            return (
-                <li>
-                    <del>{todo.text}</del> <a href="" onClick={this.done.bind(this)}>✓</a>
-                    <small style={{color:'green'}}>Hamza</small>
-                </li>
-            );
-        } else {
-            return (
-                <li>
-                    {todo.text} <a href="" onClick={this.done.bind(this)}>✓</a>
-                </li>
-            );
-        }
+        let item = this.props.item
+        console.log(this.props.item)
+        return (
+            <li>
+                <del key={this.props.key} id={this.props.ownerid}>
+                    {this.props.itemname} 
+                    {this.props.description}
+                    {this.props.quantity}
+                </del> 
+                    <a href="" onClick={this.itemCheckedOff}>✓</a>
+                <small style={{color:'green'}}>Hamza</small>
+            </li>
+        );
     }
 }
 
+// this componente gets rendered to DOM
+// passes createItem function as prop to NewItem Class so users can add additional items to event
+// passes toggleTask function as prop to ItemList
 export default class ItemsCheckList extends Component {
     constructor() {
         super();
-        
         this.state = {
-            todos: [
-                {
-                    text: 'Default Test value',
-                    done: false
-                },
-                // {
-                //     text: 'Done thing',
-                //     done: true
-                // },
-                // {
-                //     text: 'Other stuff',
-                //     done: false
-                // }
-            ]
-        };
+            items: [],
+        }
     }
-    
-    createTask(text) {
-        this.state.todos.push({
-            text,
+    // receives list of items for event as props from Welcome.js
+
+    // passed to NewItem class above
+    createItem = (text, quantity, description) => {
+        this.items.push({
+            itemname: text,
+            quantity: quantity,
+            description: text,
             done: false
         });
-        
-        this.setState({todos: this.state.todos});
+        console.log(this.items)
     }
-    
-    toggleTask(todo) {
-        let task = _.find(this.state.todos, todo);
+
+    // passed to ItemList class above
+    // sets state if item has been checked or not
+    toggleTask = (item) => {
+        let task = _.find(this.props.items, item);
         task.done = !task.done;
-        this.setState({todos: this.state.todos});
+        this.setState({items: this.state.items});
     }
     
     render() {
         return (
             <div>
-                <NewTodo createTask={this.createTask.bind(this)} />
-                <TodoList todos={this.state.todos} toggle={this.toggleTask.bind(this)} />
+                <NewItem createItem={this.createItem} />
+                <ItemList items={this.props.items} toggle={this.toggleTask}/>
             </div>
         );
     }
